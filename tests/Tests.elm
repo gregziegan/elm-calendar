9051e4de6cfd28553c00a987c44a045f58a9442b
Greg Ziegan <greg.ziegan@gmail.com>
2016-10-08 23:16:23 -0700

    add week range test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -7,17 +7,10 @@ import Date exposing (..)
 import Date.Extra as Date exposing (..)
 
 
-dayPrior : Date
-dayPrior =
-    start
-        |> Date.add Day -1
-
-
 start : Date
 start =
     Date.fromParts 2016 Oct 4 14 30 0 0
         |> Date.add Day 1
-        |> Debug.log "start"
 
 
 end : Date
@@ -26,9 +19,27 @@ end =
         |> Date.add Hour 5
 
 
+dayPrior : Date
+dayPrior =
+    start
+        |> Date.add Day -1
+
+
 dayAfter : Date
 dayAfter =
     Date.add Day 1 start
+        |> Date.add Hour 2
+
+
+weekPrior : Date
+weekPrior =
+    Date.add Week -1 start
+        |> Date.add Hour -2
+
+
+weekAfter : Date
+weekAfter =
+    Date.add Week 1 start
         |> Date.add Hour 2
 
 
@@ -38,19 +49,45 @@ viewing =
         |> Date.add Hour 1
 
 
+dayRangeDescriptionTests : Test
+dayRangeDescriptionTests =
+    describe "describe how the event spans a day view"
+        [ test "event that falls within a day of date is described as StartsAndEnds"
+            <| \() ->
+                Expect.equal StartsAndEnds (Event.rangeDescription start end Day viewing)
+        , test "event that starts on date and continues after the day described as ContinuesAfter"
+            <| \() ->
+                Expect.equal ContinuesAfter (Event.rangeDescription start dayAfter Day viewing)
+        , test "event that ends on date and beings prior to the day described as ContinuesPrior"
+            <| \() ->
+                Expect.equal ContinuesPrior (Event.rangeDescription dayPrior end Day viewing)
+        , test "event that continues prior and after the day described as ContinuesAfterAndPrior"
+            <| \() ->
+                Expect.equal ContinuesAfterAndPrior (Event.rangeDescription dayPrior dayAfter Day viewing)
+        ]
+
+
+weekRangeDescriptionTests : Test
+weekRangeDescriptionTests =
+    describe "describe how the event spans a month's week view"
+        [ test "event that falls within a week of date is described as StartsAndEnds"
+            <| \() ->
+                Expect.equal StartsAndEnds (Event.rangeDescription start end Week viewing)
+        , test "event that starts on date and continues after the day described as ContinuesAfter"
+            <| \() ->
+                Expect.equal ContinuesAfter (Event.rangeDescription start weekAfter Week viewing)
+        , test "event that ends on date and beings prior to the day described as ContinuesPrior"
+            <| \() ->
+                Expect.equal ContinuesPrior (Event.rangeDescription weekPrior end Week viewing)
+        , test "event that continues prior and after the day described as ContinuesAfterAndPrior"
+            <| \() ->
+                Expect.equal ContinuesAfterAndPrior (Event.rangeDescription weekPrior weekAfter Week viewing)
+        ]
+
+
 all : Test
 all =
     describe "range descriptions accurately describe how the event spans an interval"
-        [ test "event that falls within an interval of date is described as StartsAndEnds"
-            <| \() ->
-                Expect.equal StartsAndEnds (Event.rangeDescription start end Day viewing)
-        , test "event that starts on date and continues after the interval described as ContinuesAfter"
-            <| \() ->
-                Expect.equal ContinuesAfter (Event.rangeDescription start dayAfter Day viewing)
-        , test "event that ends on date and beings prior to the interval described as ContinuesPrior"
-            <| \() ->
-                Expect.equal ContinuesPrior (Event.rangeDescription dayPrior end Day viewing)
-        , test "event that continues prior and after the date and interval described as ContinuesAfterAndPrior"
-            <| \() ->
-                Expect.equal ContinuesAfterAndPrior (Event.rangeDescription dayPrior dayAfter Day viewing)
+        [ dayRangeDescriptionTests
+        , weekRangeDescriptionTests
         ]
